dd6789e92c6ca3dde742219b3d13965cf5aa7b9b
Lyssa Asus <gilyssabrito@gmail.com>
2023-05-20 18:20:48 -0300

    config provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,8 +9,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(UrlGenerator $url)
     {
-        if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
-        }
     }
 }
